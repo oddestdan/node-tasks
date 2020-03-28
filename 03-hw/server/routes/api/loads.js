398eafc0976c057925fc82d1d55a178b55d6c8f5
Dan <oddestdan@gmail.com>
2020-03-28 02:03:16 +0200

    Create, view loads, update and delete those with status NEW

---
+++ 03-hw/server/routes/api/loads.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const router = express.Router();
+
+const Load = require('../../models/Load');
+const User = require('../../models/User');
+
+const getCreatorIdByUser = async user => {
+  const { username, password } = user;
+  return (await User.findOne({ username, password }))._id;
+};
+
+// // Get All Loads
+// router.get('/loads', (req, res) => {
+//   Load.find({})
+//     .then(loads => res.json({ status: 'ok', loads }))
+//     .catch(e => {
+//       res.status(500).json({ status: e.message });
+//     });
+// });
+
+// Get Created Loads
+router.get('/loads', async (req, res) => {
+  const creatorId = await getCreatorIdByUser(req.user);
+
+  Load.find({ creatorId })
+    .then(loads => res.json({ status: 'ok', loads }))
+    .catch(e => {
+      res.status(500).json({ status: e.message });
+    });
+});
+
+// Get Load
+router.get('/loads/:id', (req, res) => {
+  Load.findById(req.params.id)
+    .then(load => res.json({ status: 'ok', load }))
+    .catch(e => {
+      res.status(500).json({ status: e.message });
+    });
+});
+
+// Create Load
+router.post('/loads', async (req, res) => {
+  // creatorId: Assigned by current User-Shipper
+  // assigneeId: default ''
+  // logs: default []
+  // status: default 'NEW'
+  // state: default ''
+  // dimensions: Specified at UI (Object)
+  // payload: Specified at UI (Number)
+
+  console.log('USER:');
+  console.dir(req.user);
+
+  const { dimensions, payload } = req.body;
+  const creatorId = await getCreatorIdByUser(req.user);
+
+  const load = new Load({ dimensions, payload, creatorId });
+
+  load
+    .save()
+    .then(() => {
+      res.json({ status: 'New load created', load });
+    })
+    .catch(e => {
+      res.status(500).json({ status: e.message });
+    });
+});
+
+// Update Load if NEW
+router.put('/loads/:id', (req, res) => {
+  Load.findByIdAndUpdate(req.params.id, req.body)
+    .then(load => {
+      if (load.status === 'NEW') {
+        res.json({ status: 'ok', load });
+      } else {
+        res.status(400).json({ status: 'Load is no longer NEW' });
+      }
+    })
+    .catch(e => {
+      res.status(500).json({ status: e.message });
+    });
+});
+
+// Update Load status
+router.patch('/loads/status/:id', (req, res) => {
+  Load.findByIdAndUpdate(req.params.id, {
+    status: req.body.status
+  })
+    .then(load => res.json({ status: 'ok', load }))
+    .catch(e => {
+      res.status(500).json({ status: e.message });
+    });
+});
+
+// TODO: Update Load assignee to appropriate Driver
+// (Automatic by the system when Shipper posts a Load)
+router.patch('/loads/assign/:id', (req, res) => {
+  // Find Load by Id
+  // Find User that matches with current load dimensions
+  // Update the Load.assigneeId with User._id
+
+  Load.findById(req.params.id)
+    .then(load => {
+      const dimensionsLoad = load.dimensions;
+      const matchDims = (dim1, dim2) => {
+        // some logic for matching dimensions
+        // width, length and height
+        return true;
+      };
+
+      res.json({ status: 'ok', load });
+    })
+    .catch(e => {
+      res.status(500).json({ status: e.message });
+    });
+});
+
+// Delete Load if NEW
+router.delete('/loads/:id', (req, res) => {
+  Load.findByIdAndDelete(req.params.id)
+    .then(load => {
+      if (load.status === 'NEW') {
+        res.json({ status: 'ok' });
+      } else {
+        res.status(400).json({ status: 'Load is no longer NEW' });
+      }
+    })
+    .catch(e => {
+      res.status(500).json({ status: e.message });
+    });
+});
+
+module.exports = router;
