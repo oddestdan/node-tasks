634e32dc28dd6eeb03d76fe570c420d7cd223e81
Dan <oddestdan@gmail.com>
2020-03-31 18:07:01 +0300

    Add global schema-related variables for scaling purposes

---
+++ 03-hw/server/globals/statuses.js
@@ -0,0 +1,14 @@
+const statuses = {
+  truck: {
+    inService: 'IS',
+    onLoad: 'OL'
+  },
+  load: {
+    new: 'NEW',
+    posted: 'POSTED',
+    assigned: 'ASSIGNED',
+    shipped: 'SHIPPED'
+  }
+};
+
+module.exports = statuses;
