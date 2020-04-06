5967e13818092747b139f115009cbb7013443912
Dan <oddestdan@gmail.com>
2020-04-06 19:25:08 +0300

    Add UI PDF logs file generation using blobs

---
+++ 03-hw/client/src/helpers/downloadBlob.js
@@ -0,0 +1,9 @@
+export const downloadBlob = (blob, filename) => {
+  const link = document.createElement('a');
+  link.href = window.URL.createObjectURL(blob);
+  link.download = filename;
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
--- 03-hw/client/src/views/Loads.vue
+++ 03-hw/client/src/views/Loads.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="loads-page">
     <div class="container">
+
       <div class="row">
         <div class="col">
           <div class="row">
@@ -12,6 +13,7 @@
             >Create New Load</button>
           </div>
           <section class="panel panel-success" v-if="loads.length">
+
             <table class="table table-striped">
               <tr>
                 <th>Load ID</th>
@@ -22,6 +24,7 @@
                 <th>Payload</th>
                 <th>Logs</th>
               </tr>
+
               <tr v-for="(load, i) in loads" :key="`${load._id}_${i}`">
                 <td>{{ load._id }}</td>
                 <td>{{ isDriver ? load.creatorId : load.assigneeId }}</td>
@@ -37,8 +40,7 @@
                     v-for="state in Object.values(loadStates)"
                     :key="`${state}`"
                     :value="state"
-                    >{{ state }}</option
-                  >
+                  >{{ state }}</option>
                 </select>
                 <td v-else>{{ load.state }}</td>
                 <td class="dimensions">
@@ -46,29 +48,11 @@
                   {{ load.dimensions['height'] }} x
                   {{ load.dimensions['length'] }}
                 </td>
-                <!-- <td>
-                  <span
-                    class="dimensions"
-                    v-for="dim in Object.keys(load.dimensions)"
-                    :key="`${dim}`"
-                  >{{ load.dimensions[dim] }} x</span>
-                </td>-->
-                <!-- <td>
-                  <p
-                    class="dimensions"
-                    v-for="dim in Object.keys(load.dimensions)"
-                    :key="`${dim}`"
-                  >{{ `${dim}: ${load.dimensions[dim]}` }}</p>
-                </td>-->
                 <td>{{ load.payload }}</td>
                 <td class="actions">
                   <span v-if="load.logs.length === 0">-</span>
-                  <!-- @click="() => handleShowLogsClick(load.logs)" -->
-
-                  <!-- modal-tall -->
-                  <!-- modal-scrollable -->
                   <b-button
-                    @click="modalLogs = load.logs"
+                    @click="currentLoad = load"
                     v-b-modal.modal-xl
                     variant="link"
                     v-else
@@ -83,6 +67,7 @@
                 </td>
               </tr>
             </table>
+
           </section>
           <section class="panel panel-danger" v-else>
             <p>No loads {{ isDriver ? 'assigned' : 'created' }} yet...</p>
@@ -90,37 +75,27 @@
         </div>
       </div>
 
-      <b-modal id="modal-xl" size="xl" title="Logs">
-        <table class="table table-striped">
-          <tr>
-            <th>Message</th>
-            <th>Timestamp</th>
-          </tr>
-          <tr v-for="log in modalLogs" :key="log.time">
-            <td>{{ log.message }}</td>
-            <td>{{ formatTime(log.time) }}</td>
-          </tr>
-        </table>
+      <LogsModal :currentLoad="currentLoad" />
 
-        <template v-slot:modal-footer="{ close }">
-          <b-button variant="dark" @click="close()">Close</b-button>
-        </template>
-      </b-modal>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
-import { formatIsoStringToDate } from '../helpers';
 import { loadStates } from '../globals';
+import LogsModal from '../components/LogsModal';
 
 export default {
   name: 'LoadsPage',
 
+  components: {
+    LogsModal,
+  },
+
   data() {
     return {
-      modalLogs: [],
+      currentLoad: null,
     };
   },
 
@@ -136,7 +111,7 @@ export default {
   methods: {
     ...mapActions(['getLoads', 'updateLoadState', 'postLoad', 'removeLoad']),
 
-    handleCreateLoadClick(e) {
+    handleCreateLoadClick() {
       this.$router.push('/loads/new');
     },
 
@@ -152,10 +127,6 @@ export default {
 
     handleDeleteLoadClick(id) {
       this.removeLoad(id);
-    },
-
-    formatTime(time) {
-      return formatIsoStringToDate(time);
     },
   },
 
